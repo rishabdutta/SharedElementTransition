5c21cbb77831cf7d719cf36198ea8ca43fba1f2c
rishab.dutta@robosoftin.com <rishab.dutta@robosoftin.com>
2018-03-13 12:30:21 +0530

    Initial Commit

---
+++ ListToDetail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ListToDetail
+//
+//  Created by Rishab Dutta on 13/03/18.
+//  Copyright © 2018 Rishab Dutta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
